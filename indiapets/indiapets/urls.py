d295d21d7bd48f8cd0c8f28f23f82fbb4032cc33
SatyaRaj <vineelraj19@gmail.com>
2020-04-03 14:51:03 +0530

    1st work

--- indiapets/indiapets/urls.py
+++ indiapets/indiapets/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path
 from accounts import views
+from feedback import views as feedback_views
 from django.conf.urls import url,include 
 
 urlpatterns = [
@@ -9,4 +10,5 @@ urlpatterns = [
     # path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
     path('', views.index),
+    path('feedback/', feedback_views.feedback),
 ]
