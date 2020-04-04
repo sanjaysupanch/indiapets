84f16f4a8f06eabe6847c985f14fcb7c53e7826a
SatyaRaj <vineelraj19@gmail.com>
2020-04-04 10:24:54 +0530

    Updated feedback form UI

--- indiapets/indiapets/urls.py
+++ indiapets/indiapets/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from accounts import views
 from feedback import views as feedback_views
+from doctor import views as doctor_views
 from django.conf.urls import url,include 
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', views.index),
     path('feedback/', feedback_views.feedback),
+    path('Doctor/', doctor_views.index),
 ]
